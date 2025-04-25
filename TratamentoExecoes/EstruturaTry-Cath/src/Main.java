b91cf3b6e2cc45b80f74ae996cd8f39b76379e7b
Gabriel Prata <ghprata7245@gmail.com>
2025-04-25 09:32:43 -0300

    Incluindo Exercicio de fixacao modulo excecao

--- TratamentoExecoes/EstruturaTry-Cath/src/Main.java
+++ TratamentoExecoes/EstruturaTry-Cath/src/Main.java
@@ -3,22 +3,34 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        method1();
 
+        System.out.println("End of Program");
+    }
+
+    public static void method1() {
+        System.out.println("---METHOD1 START---");
+        method2();
+        System.out.println("---METHOD1  END---");
+    }
+
+    public static void method2() {
+        System.out.println("---METHOD2 START---");
         Scanner sc = new Scanner(System.in);
 
         try {
             String[] vect = sc.nextLine().split(" ");
             int position = sc.nextInt();
             System.out.println(vect[position]);
-        }
-        catch (ArrayIndexOutOfBoundsException e) {
+        } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Invalid position!");
-        }
-        catch (InputMismatchException e) {
+            e.printStackTrace(); //Mostra quais linhas estao com erro.
+            sc.next();
+        } catch (InputMismatchException e) {
             System.out.println("Input Error");
         }
-        
-        System.out.println("End of Program");
+
+        System.out.println("---METHOD2 END---");
 
         sc.close();
     }
